bc4e3d3c0d05f6ca86b6b1615b0185954d67fa21
Manuela Kuhn <manuela.kuhn@ovgu.de>
2020-12-01 16:43:14 +0100

    Fix paths for rsync call

--- rsync.py
+++ rsync.py
@@ -3,36 +3,65 @@ import os
 import utils
 
 
-def sync_data_via_rsync(
-    subjects: list, 
-    user: str, 
-    host: str, 
-    src_path: str, 
-    target_path: str):
+def _get_path(conf, acq):
+    user = conf.get("user", None)
+    host = conf.get("host", None)
+    path = conf["path"].format(acq)
+
+    if user is None or host is None:
+        return path
+    else:
+        return "{user}@{host}:{path}".format(user=user, host=host, path=path)
+
+
+def sync_data_via_rsync(subjects: list, config: dict):
     """Gets the data from a remote server by using rsync.
 
     Args:
         subjects: List of subjects.
-        user: The username used for logging.
-        host: The hostname of the remote server.
-        src_path: The path on the remote system to get data from.
-        target_path: The path on the local system to store the data to.
+        config: A dictionary containing the rsync parameters. It has to containt
+            the following keys:
+                src:
+                    user: The username with which get data.
+                    host: The name of the source server to get data from.
+                    path: The path on the source system to get data from.
+                dest:
+                    user: The username to store the data as.
+                    host: The name of the target server to store the data to.
+                    path: The path on the target system to store the data to.
     """
 
-    for i in subjects:
+    for key in ["src", "dest"]:
+        if key not in config["rsync"].keys():
+            raise utils.ConfigError("Missing {} configuration.".format(key))
 
-        # TODO check correct command
+    for i in subjects:
+        src = _get_path(config["rsync"]["src"], i["acq"])
+        dest = _get_path(config["rsync"]["dest"], i["acq"])
+
+        # rsync options used:
+        # -a: archive mode; equals -rlptgoD
+        #    r: recursive
+        #    l: copy symlinks as symlinks
+        #    p: preserve permissions
+        #    t: preserve modification times
+        #    g: preserve group
+        #    o: preserve owner
+        #    D: preserve device and special files
+        # -c: skip based on checksum, not mod-time and size
+        # -v: verbose
+        # --progress: show progress during transfer
+        # --info=FLAG: fine-grained informational verbosity
+        #    FLIST: Mention file-list receiving/sending (levels 1-2)
         cmd = (
-            "rsync --acvg --progress --info=FLIST0 {user}@{host}:{src_path} {target_path}"
-            .format(
-                user=user,
-                host=hostname,
-                path=data_path.format(acq=i[acq], target=dir_target)
-            )
+            "rsync -acv --progress --info=FLIST0 {src} {dest}"
+            .format(src=src, dest=dest)
         )
         print("running command:\n", cmd)
 
         os.system(cmd)
+
+        # Todo get status and return value
 
 
 if __name__ == "__main__":
